4203ca2a59799a531435c2e8aa2646acb1fbdf56
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2023-01-12 12:05:36 +0000

    add localstorage as local repository for projectList object array

--- main.js
+++ main.js
@@ -5093,6 +5093,8 @@ let clickHandler = (
 
 
 
+
+
 /***/ }),
 
 /***/ "./src/projects-tasks.js":
@@ -5112,16 +5114,17 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "updateProject": () => (/* binding */ updateProject),
 /* harmony export */   "updateTask": () => (/* binding */ updateTask)
 /* harmony export */ });
-/* harmony import */ var date_fns_parseISO__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! date-fns/parseISO */ "./node_modules/date-fns/esm/parseISO/index.js");
+/* harmony import */ var date_fns_parseISO__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! date-fns/parseISO */ "./node_modules/date-fns/esm/parseISO/index.js");
+/* harmony import */ var _storage__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./storage */ "./src/storage.js");
 
     
     
-    let projectList = [{
-        title: 'Demo Project',
-        tasks: [
-            
-        ],
-    }]
+    
+    
+    let projectList = [];
+    if ((0,_storage__WEBPACK_IMPORTED_MODULE_0__.retrieveLocalstorage)(projectList)) {
+        projectList = (0,_storage__WEBPACK_IMPORTED_MODULE_0__.retrieveLocalstorage)(projectList)
+    };
 
     class project {
         constructor(title, tasks) {
@@ -5134,14 +5137,17 @@ __webpack_require__.r(__webpack_exports__);
     function createProject(title) {
         let newProject = new project(title);
         projectList.push(newProject);
+        (0,_storage__WEBPACK_IMPORTED_MODULE_0__.updateLocalStorage)(projectList);
     };
 
     function deleteProject(index) {
         projectList.splice(index, 1);
+        (0,_storage__WEBPACK_IMPORTED_MODULE_0__.updateLocalStorage)(projectList);
     };
 
     function updateProject(title, projectIndex) {
         projectList[projectIndex].title = title;
+        (0,_storage__WEBPACK_IMPORTED_MODULE_0__.updateLocalStorage)(projectList);
     }
 
     function getProjectNames() {
@@ -5162,27 +5168,102 @@ __webpack_require__.r(__webpack_exports__);
     };
     
     function createTask(title, description, dueDate, priority, projectIndex) {
-        let newTask = new task(title, description, (0,date_fns_parseISO__WEBPACK_IMPORTED_MODULE_0__["default"])(dueDate), priority, projectIndex);
+        let newTask = new task(title, description, (0,date_fns_parseISO__WEBPACK_IMPORTED_MODULE_1__["default"])(dueDate), priority, projectIndex);
         projectList[projectIndex].tasks.push(newTask);
-        console.log(projectList)        // only to check status
+        (0,_storage__WEBPACK_IMPORTED_MODULE_0__.updateLocalStorage)(projectList);
     }
 
     function deleteTask(projectIndex, taskIndex) {
         projectList[projectIndex].tasks.splice(taskIndex, 1);
+        (0,_storage__WEBPACK_IMPORTED_MODULE_0__.updateLocalStorage)(projectList);
     }
 
     function updateTask(title, description, dueDate, priority, projectIndex, taskIndex) {
         projectList[projectIndex].tasks[taskIndex].description = description;
         projectList[projectIndex].tasks[taskIndex].title = title;
-        projectList[projectIndex].tasks[taskIndex].dueDate = (0,date_fns_parseISO__WEBPACK_IMPORTED_MODULE_0__["default"])(dueDate);
+        projectList[projectIndex].tasks[taskIndex].dueDate = (0,date_fns_parseISO__WEBPACK_IMPORTED_MODULE_1__["default"])(dueDate);
         projectList[projectIndex].tasks[taskIndex].priority  = priority;
+        (0,_storage__WEBPACK_IMPORTED_MODULE_0__.updateLocalStorage)(projectList);
     }
 
     function toggleTaskCompleteStatus(projectIndex, taskIndex){
         projectList[projectIndex].tasks[taskIndex].complete == false ? projectList[projectIndex].tasks[taskIndex].complete = true : projectList[projectIndex].tasks[taskIndex].complete = false;
+        (0,_storage__WEBPACK_IMPORTED_MODULE_0__.updateLocalStorage)(projectList);
     };
 
 
+
+
+
+/***/ }),
+
+/***/ "./src/storage.js":
+/*!************************!*\
+  !*** ./src/storage.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "retrieveLocalstorage": () => (/* binding */ retrieveLocalstorage),
+/* harmony export */   "updateLocalStorage": () => (/* binding */ updateLocalStorage)
+/* harmony export */ });
+/* harmony import */ var _projects_tasks__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./projects-tasks */ "./src/projects-tasks.js");
+
+
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+function retrieveLocalstorage(db) {
+
+    if (storageAvailable("localStorage")) {
+        if (localStorage.getItem("projectList") === null) {
+
+            // no database, creating a new one
+
+            localStorage.setItem("projectList", JSON.stringify(db))
+            // createProject("Demo Project")
+            // createTask("Example project, non urgent, next week", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a luctus dui, sed rhoncus est. Nulla bibendum metus non quam viverra iaculis. Aliquam erat volutpat. Aenean in tempus lacus. Nullam et tortor porta, facilisis ante a, vehicula ligula. Integer ex magna, fringilla eget orci a, feugiat accumsan ligula. Ut vehicula tempus elementum.", "2023-01-16", "false", 0)
+            // createTask("Urgent project for today", "Fusce et porta arcu. Proin bibendum ornare mauris sit amet vestibulum. Nunc euismod sagittis posuere. Donec non tempor mi, eu feugiat felis. Nulla ut justo viverra, eleifend odio sit amet, hendrerit felis. Nulla egestas, odio id faucibus faucibus, lectus orci finibus ex, vel pharetra ligula enim ut leo.", "2023-01-11", "true", 0)
+            // createTask("Project for this week, also urgent", "Donec ante lorem, eleifend sed velit ac, lacinia volutpat libero. Duis lorem justo, elementum ac tristique in, dapibus at tellus. Nam lectus turpis, placerat ut urna porta, ornare feugiat enim. Sed pretium enim nulla, at vulputate libero porta sed. In hac habitasse platea dictumst. Nam eget egestas nisl, in pellentesque quam.", "2023-01-14", "true", 0)
+            return false
+        }
+        else {
+            let retrievedList = JSON.parse(localStorage.getItem("projectList"))
+            return retrievedList;
+        
+        }
+        }
+    else {
+        alert("no storage available")
+    }
+}
+
+function updateLocalStorage (db) {
+    localStorage.setItem("projectList", JSON.stringify(db))
+}
 
 
 
@@ -5269,18 +5350,13 @@ var __webpack_exports__ = {};
   \**********************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _src_styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../src/styles.css */ "./src/styles.css");
-/* harmony import */ var _projects_tasks__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./projects-tasks */ "./src/projects-tasks.js");
-/* harmony import */ var _dom_helper__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./dom-helper */ "./src/dom-helper.js");
+/* harmony import */ var _dom_helper__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./dom-helper */ "./src/dom-helper.js");
 
 
-
-(0,_dom_helper__WEBPACK_IMPORTED_MODULE_2__.renderHeader)();
-(0,_dom_helper__WEBPACK_IMPORTED_MODULE_2__.renderStaticElements)();
-(0,_dom_helper__WEBPACK_IMPORTED_MODULE_2__.renderProjectSidebar)();
-(0,_projects_tasks__WEBPACK_IMPORTED_MODULE_1__.createTask)("title1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a luctus dui, sed rhoncus est. Nulla bibendum metus non quam viverra iaculis. Aliquam erat volutpat. Aenean in tempus lacus. Nullam et tortor porta, facilisis ante a, vehicula ligula. Integer ex magna, fringilla eget orci a, feugiat accumsan ligula. Ut vehicula tempus elementum.", "2023-01-13", "false", 0)
-;(0,_projects_tasks__WEBPACK_IMPORTED_MODULE_1__.createTask)("title2", "description2", "2023-01-11", "true", 0)
-;(0,_projects_tasks__WEBPACK_IMPORTED_MODULE_1__.createTask)("title3", "description3", "2023-01-12", "false", 0)
-;(0,_dom_helper__WEBPACK_IMPORTED_MODULE_2__.renderTaskList)();
+(0,_dom_helper__WEBPACK_IMPORTED_MODULE_1__.renderHeader)();
+(0,_dom_helper__WEBPACK_IMPORTED_MODULE_1__.renderStaticElements)();
+(0,_dom_helper__WEBPACK_IMPORTED_MODULE_1__.renderProjectSidebar)();
+(0,_dom_helper__WEBPACK_IMPORTED_MODULE_1__.renderTaskList)();
 })();
 
 /******/ })()
