a0d763b8838170f79c286b171a5c0c43ec2fc831
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2023-01-15 16:51:33 +0000

    fix button toggle when screen is less than 600px

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import css from '../src/styles.css';
-import { renderStaticElements, renderHeader, renderProjectSidebar, renderTaskList } from './dom-helper';
+import { responsiveSidebar,renderStaticElements, renderHeader, renderProjectSidebar, renderTaskList } from './dom-helper';
 renderHeader();
 renderStaticElements();
 renderProjectSidebar();
 renderTaskList();
-let mediaQuery = window.matchMedia('(min-width: 768px)')
+let mediaQuery = window.matchMedia('(max-width: 768px)')
 
 
-  // Register event listener
-//   mediaQuery.addEventListener("change", () => {
-//     responsiveSidebar(mediaQuery)
-//   })
+  mediaQuery.addEventListener("change", () => {
+    responsiveSidebar(mediaQuery)
+  })
 
-//   window.addEventListener('DOMContentLoaded', (event) => {
-//     responsiveSidebar(mediaQuery)
-// });
+  window.addEventListener('DOMContentLoaded', (event) => {
+    responsiveSidebar(mediaQuery)
+});
   
-//   responsiveSidebar(mediaQuery);
+  responsiveSidebar(mediaQuery);
